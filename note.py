def194e6a07169883112bb18f536224a43821f05
lucko07 <frantz1111@gmail.com>
2025-11-16 20:07:51 -0500

    Added note.py

---
+++ note.py
@@ -0,0 +1,19 @@
+# how to push  files to GitHub after you done.
+"""
+✅Step 1 - Make sure you're inside the project folder.
+✅ STEP 2 — Check what changed.
+git status  (That's a command to check.) You should see your Day 3 files in red (untracked) or green (modified).
+✅ STEP 3 — Add ALL new files by using the command below:
+git add .
+✅ STEP 4 — Commit the changes, Using the command below:
+git commit -m "Completed Day 3 (if else, loops, while loops)"
+✅ STEP 5 — Push to GitHub, using the command below:
+git push
+If your branch is called "main" and Git asks you to set upstream, then do:
+git push -u origin main
+After this, pushes will always be simple:
+git push
+✅ STEP 6 — Verify on GitHub
+👉 https://github.com/lucko07/Learning_Python
+"""
+
